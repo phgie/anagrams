65a10bf08fd2aedb56962be4e6a5fdfc65f8caa1
phgie <philipp.giesen@gmail.com>
2024-02-27 16:35:36 +0100

    add anagram implementation for default, non-whitespace and special character anagrams

--- src/main/java/de/anagrams/AnagramChecker.java
+++ src/main/java/de/anagrams/AnagramChecker.java
@@ -1,11 +1,35 @@
 package de.anagrams;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class AnagramChecker {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String... args) {
+        boolean isAnagram = args.length == 2 && isAnagram(args[0], args[1]);
+        System.out.println("Given String Arguments are" + (isAnagram ? "" : " NOT") + " an anagram of each other!");
     }
 
     protected static boolean isAnagram(String string1, String string2) {
-        return false;
+        return sort(string1.toLowerCase()).equals(sort(string2.toLowerCase()));
+    }
+
+    private static String sort(String string) {
+        return Arrays.stream(string.toLowerCase().split("")).sorted().collect(Collectors.joining());
+    }
+
+    protected static boolean isAnagramIgnoringWhitespaces(String string1, String string2) {
+        return isAnagram(removeWhitespaces(string1), removeWhitespaces(string2));
+    }
+
+    private static String removeWhitespaces(String string) {
+        return string.replaceAll("\\s", "");
+    }
+
+    protected static boolean isAnagramIgnoringSpecialCharacters(String string1, String string2) {
+        return isAnagram(removeSpecialCharacters(string1), removeSpecialCharacters(string2));
+    }
+
+    private static String removeSpecialCharacters(String string) {
+        return string.replaceAll("[^A-Za-z0-9]", "");
     }
 }
--- src/test/java/de/anagrams/AnagramCheckerTest.java
+++ src/test/java/de/anagrams/AnagramCheckerTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static de.anagrams.AnagramChecker.isAnagram;
+import static de.anagrams.AnagramChecker.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
@@ -18,28 +18,53 @@ class AnagramCheckerTest {
         assertTrue(isAnagram(string1, string2));
     }
 
+    @ParameterizedTest
+    @MethodSource("getAnagramsWithWhitespaces")
+    void isAnagramIgnoringWhitespacesTest(String string1, String string2) {
+        assertTrue(isAnagramIgnoringWhitespaces(string1, string2));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getAnagramsWithSpecialCharacters")
+    void isAnagramIgnoringSpecialCharactersTest(String string1, String string2) {
+        assertTrue(isAnagramIgnoringSpecialCharacters(string1, string2));
+    }
+
     private static Stream<Arguments> getAnagrams() {
         return Stream.of(
-                arguments("New York Times", "monkeys write"),
-                arguments("Church of Scientology", "rich-chosen goofy cult"),
-                arguments("McDonald's restaurants", "Uncle Sam's standard rot"),
+                // same string length, same characters
                 arguments("coronavirus", "carnivorous"),
-                arguments("She Sells Sanctuary", "Santa; shy, less cruel"),
-                arguments("She Sells Sanctuary", "Satan; cruel, less shy"),
                 arguments("evil", "vile"),
                 arguments("a gentleman", "elegant man"),
                 arguments("silent", "listen"),
                 arguments("restful", "fluster"),
                 arguments("cheater", "teacher"),
+                arguments("Santa", "Satan"),
+                arguments("Madam Curie", "Radium came")
+        );
+    }
+
+    private static Stream<Arguments> getAnagramsWithWhitespaces() {
+        return Stream.of(
+                // addition/removal of spaces
+                arguments("New York Times", "monkeys write"),
+                arguments("McDonald's restaurants", "Uncle Sam's standard rot"),
                 arguments("funeral", "real fun"),
                 arguments("adultery", "true lady"),
                 arguments("forty five", "over fifty"),
-                arguments("Santa", "Satan"),
                 arguments("William Shakespeare", "I am a weakish speller"),
-                arguments("Madam Curie", "Radium came"),
                 arguments("George Bush", "He bugs Gore"),
                 arguments("Tom Marvolo Riddle", "I am Lord Voldemort"),
                 arguments("Anagrams", "Ars magna")
+        );
+    }
+
+    private static Stream<Arguments> getAnagramsWithSpecialCharacters() {
+        return Stream.of(
+                // addition/removal of non-alphanumeric characters
+                arguments("Church of Scientology", "rich-chosen goofy cult"),
+                arguments("She Sells Sanctuary", "Santa; shy, less cruel"),
+                arguments("Satan; cruel, less shy", "She Sells Sanctuary")
         );
     }
 }
